ccbfd04319911b8b777852ee1e2dce9e6466ea85
Anna Ivanova <annaiv@mit.edu>
2020-05-30 00:17:26 -0400

    EventsRev analyses

--- 1_EventsRev_plausibility/analyses/analyze_data.R
+++ 1_EventsRev_plausibility/analyses/analyze_data.R
@@ -1,28 +1,29 @@
+# Created on 2020-05-26 by Anna Ivanova
+# Based on the code by Rachel Ryskin
+
 rm(list=ls())
 library(tidyverse)
 library(stringr)
 library(stringi)
 
-# CHANGE THE PATH TO FOLDER WHERE ALL THE TURK OUTPUTS ARE
-#filename='Batch_4055141_batch_results.csv'
-filename='Batch_4057413_batch_results.csv'
+# READ DATA
+filenames=c('../results_raw/Batch_4055141_batch_results.csv',
+  '../results_raw/Batch_4055390_batch_results.csv',
+  '../results_raw/Batch_4055655_batch_results.csv',
+  '../results_raw/Batch_4057413_batch_results.csv')
 
-maxna = .1 # maximum permitted proportion of NA responses per rating question
-#mincorrect = .75 # minimum permitted proportion of correct responses to comprehension questions
+data <- lapply(filenames, read.csv)
+data = do.call("rbind", data)
+            
 num.trials = 42  # maximum number of trials per participant
-
-#data = read_csv(file.path(data_path, filename)) %>%
-data = read_csv(filename) %>%
-  mutate(batch.name = filename)
 
 # only keep WorkerId and cols that Start with Answer or Input
 data = data %>% select(starts_with('Input'),starts_with('Answer'),starts_with('WorkerId')) 
 
-# gather (the list of columns may need to be changed depending on what questions you asked)
+# gather (specify the list of columns you need)
 data = data %>% gather(key='variable',value="value",
                        -WorkerId,-Input.list,-Answer.country,
-                       -Answer.English,-Answer.answer
-                       )
+                       -Answer.English,-Answer.answer)
 
 # separate
 data = data %>% separate(variable, into=c('Type','TrialNum'),sep='__',convert=TRUE) 
@@ -30,12 +31,11 @@ data = data %>% separate(variable, into=c('Type','TrialNum'),sep='__',convert=TR
 # spread
 data = data %>% spread(key = Type, value = value)
 
-# exclude bad workers
+# exclude bad workers (note: currently done manually)
 data = data %>%
-  filter(!(WorkerId %in% c('AT8S19U5993HR', 'A2R1A479K07ME5')))
+  filter(!(WorkerId %in% c('AT8S19U5993HR', 'A2R1A479K07ME5')))                   # bad responses
 
-## Ways to summarize ratings data if that is your DV
-
+## Summarize ratings data 
 data$Answer.Rating <- as.numeric(data$Answer.Rating)
 
 data = data %>% 
@@ -47,21 +47,14 @@ data$xx2 = NULL
 ## SAVE A LONGFORM VERSION OF YOUR DATA
 write_csv(data,"longform_data.csv")
 
-#%>% 
-#  mutate(
-#  Correct = correct.answer == Answer.YNQ)
-#warning here is because there end up being NAs in the lengthCOnd column for the filler trials
 
-z_score = function(xs) {
-  (xs - mean(xs)) / sd(xs)
-}
+# ANALYSES
 
-## Add participant correct and na, z score ratings
-data_accuracy = data %>% 
+## Look at data by participant (TODO: add avg rating for plaus and implaus)
+data = data %>% 
   group_by(WorkerId) %>%
   mutate(
     na.pct = mean(is.na(Answer.Rating)),
-    correct.pct = mean(Correct,na.rm=TRUE),
     n = length(Answer.Rating)) %>%
   ungroup()
 
@@ -69,55 +62,53 @@ data_summ = data %>%
   group_by(WorkerId) %>%
   summarize(
     na.pct = mean(is.na(Answer.Rating)),
-    correct.pct = mean(Correct,na.rm=TRUE),
     n = length(Answer.Rating))
 
 ## save a summary of individual subjects' performance
-#write_csv(data_summ,"/Users/rachelryskin/Dropbox (MIT)/psycholinguistics_lab/my_files/turk_expt_in_class/example_data_summ.csv")
+write_csv(data_summ,"data_summ_by_worker.csv")
 
+z_score = function(xs) {
+  (xs - mean(xs)) / sd(xs)
+}
 
-#filter for US, English, na, and correct and duplicate, then get z-scores
-data.good = data_accuracy %>%
-  filter(na.pct <= maxna &
-           correct.pct >= mincorrect &
-           Answer.English == "yes" &
-           Answer.country == "USA" &
-           n <= num.trials) %>%
-  filter(!is.na(Answer.Rating)) %>% 
-  group_by(WorkerId) %>% 
-  mutate(
-    participant.z = z_score(as.numeric(Answer.Rating)))%>% 
-  ungroup()
+#filter for US, English, na, and duplicate, then get scores
+data$Item = as.numeric(data$Item)
+data.good = data %>%
+  filter(Answer.English == "yes" &
+         Answer.country == "USA" &
+         n <= num.trials) %>%
+  filter(!is.na(Answer.Rating)) %>%
+  filter(Item<=40)      # remove attention check items
 
-data.good.summary = data.good %>% 
-  filter(trialType == 'particle-length') %>% 
-  group_by(shiftCond,lengthCond) %>% 
+data.good.summary = data.good %>%
+  group_by(Plausibility, Item) %>%
   summarize(
-    m = mean(participant.z),
-    stdev= sd(participant.z),
-    se = stdev/sqrt(n()),
-    upper= m+se*1.96,
-    lower=m-se*1.96,
-    m.r = mean(Answer.Rating),
-    stdev.r= sd(Answer.Rating),
-    se.r = stdev.r/sqrt(n()),
-    upper.r= m.r+se.r*1.96,
-    lower.r=m.r-se.r*1.96
+    m = mean(Answer.Rating),
+    stdev= sd(Answer.Rating),
+#    se = stdev/sqrt(n()),
+#    upper= m+se*1.96,
+#    lower=m-se*1.96
   )
   
-# graphs of raw ratings and z-scores by condition 
+## save a summary of individual subjects' performance
+data.good.summary = data.good.summary[,c(2,1,3,4)]
+write_csv(data.good.summary[order(data.good.summary$Item),],
+          "EventsRev_data_summ.csv")
+
+# graphs of ratings by condition 
 p1 = ggplot(data=data.good.summary)+
-  geom_bar(aes(x=shiftCond,y=m),stat='identity')+
-  geom_errorbar(aes(x=shiftCond,ymin=lower,ymax=upper),width = .2)+
-  facet_wrap(~lengthCond)+
-  ylab('average z-scores')
+  stat_summary(mapping = aes(x = Plausibility, y = m), 
+               geom = 'col', fun.y = 'mean', color = 'black')+
+  geom_point(mapping = aes(x = Plausibility, y = m), 
+             shape=21, size=2, alpha=0.5, stroke=1.5,
+             position=position_jitter(width=0.15, height=0),
+             show.legend = FALSE)+
+  stat_summary(mapping = aes(x = Plausibility, y = m), 
+               geom = 'errorbar', fun.data = 'mean_se', 
+               color = 'black', size = 1.5, width=0.3)+
+  ylab('Plausibility')+
+  theme_classic()
 
-p1
+ggsave('EventsRev plaus plot.png', p1, width=10, height=10, units="cm")
 
-p2 = ggplot(data=data.good.summary)+
-  geom_bar(aes(x=shiftCond,y=m.r),stat='identity')+
-  geom_errorbar(aes(x=shiftCond,ymin=lower.r,ymax=upper.r),width = .2)+
-  facet_wrap(~lengthCond)+
-  ylab('average raw ratings')
-
-p2
+print(p1)
